0d69035f66673bcff18dd6c242d509f075771b80
Lukas <fischi.ftw@gmail.com>
2019-12-28 20:12:58 +0100

    simple changing color loop

---
+++ src/colorLoop.ts
@@ -0,0 +1,30 @@
+import sleep from "./sleep"
+
+let running = true
+let color = "000000"
+
+let i = 0
+
+function getRunning() {
+    return running
+}
+
+function setRunning(val: boolean) {
+    running = val
+    if(running)
+        run()
+}
+
+function setColor(val: string) {
+    color = val
+}
+
+async function run() {
+    while(running) {
+        console.log(i++)
+        console.log(color)
+        await sleep(100)
+    }
+}
+
+export {getRunning, setRunning, setColor, run}
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,47 @@
 import bodyParser from 'body-parser'
 import express, { Application, Request, Response } from 'express'
-import asdf from './asdf'
+import { check, validationResult } from 'express-validator'
+import { run, setColor, setRunning, getRunning } from './colorLoop'
 
 const app : Application = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(`TS App is Running, Hello ${asdf(799)}`)
+app.get("/toggle", (req, res) => {
+    setRunning(!getRunning())
+    
+    res.status(200).send({running: getRunning()})
 })
 
-const PORT = process.env.PORT ?? 3000
+app.post(
+    "/set",
+    [
+        check("color").matches(/^#?[0-9A-Fa-f]{6}$/).withMessage("not a color")
+    ],
+    (req: Request , res: Response) => {
 
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            return res.status(400).json({ errors: errors.array() });
+
+        let color = req.body.color as string;
+
+        if(color.startsWith("#"))
+            color = color.replace("#", "")
+
+        color = color.toUpperCase()
+
+        setColor(color)
+
+        console.log(req.body)
+        res.status(200).send({message: "ok"})
+    }
+)
+
+const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT,() => {
     console.log(`server is running on PORT ${PORT}`)
 })
+
+run()
